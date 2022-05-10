9556d69c5d9b582357f1bb51e3417a37bdbfb12c
Fabiano Frank <68971295+fabianofrank@users.noreply.github.com>
2022-05-10 14:21:59 -0300

    Add index
    Add index to all tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -36,3 +36,10 @@ CREATE TABLE invoices (
   FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
   PRIMARY KEY(id)
 );
+
+CREATE INDEX ON invoices (medical_history_id);
+CREATE INDEX ON medical_histories (patient_id);
+CREATE INDEX ON invoice_items (invoice_id);
+CREATE INDEX ON invoice_items (treatment_id);
+CREATE INDEX ON treatments_history (medical_history_id);
+CREATE INDEX ON treatments_history (treatment_id);
